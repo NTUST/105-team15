8ffdf1eb54e414334e0239f642d0564dbdaa74c5
KamiSP <apple47145@gmail.com>
2016-06-03 16:57:32 +0800

    Add Dadabase and fix something!

--- branch/back/MRT_HotSpot/MRTapps/views.py
+++ branch/back/MRT_HotSpot/MRTapps/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World!")
+
+#def get_name(request):
+#    if request.method == 'POST':
+#        form = NameForm(request.POST)
+#
+#        if form.is_valid():
+#            return HttpResponseRedirect('/thanks/')
+#
+#    else:
+#        form = NameForm()
+#
+#    return render(request, 'name.html', {'form':form})
